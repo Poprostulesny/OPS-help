e9c751904a597a15de9a26c10c45cc17e1ef496a
Mateusz <mateusz.lesniczak@gmail.com>
2025-12-08 23:38:15 +0100

    task2

--- LABS2/Task2/task2.c
+++ LABS2/Task2/task2.c
@@ -10,20 +10,23 @@
 #include <signal.h>
 #include <bits/sigaction.h>
 #include <sys/wait.h>
-#define RAND_MAX 100
+// Na początku pliku, po #define ERR:
+#define SIG_STUDENT SIGRTMIN
+#define SIG_TEACHER (SIGRTMIN + 1)
 
 #define ERR(source) \
     (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
 
 volatile sig_atomic_t lastsignal = 0;
-volatile sig_atomic_t lastsender = 0;
-volatile sig_atomic_t lastreturn = 0;
 
-int childcount=0;
-  int *probabilities;
-    int *student_pid;
-    int *student_exit;
-    int alive_children;
+volatile sig_atomic_t pending_students[100];
+volatile sig_atomic_t pending_head = 0;
+volatile sig_atomic_t pending_tail = 0;
+int childcount = 0;
+int *probabilities;
+int *student_pid;
+int *student_exit;
+int alive_children;
 
 int checked_strtol(char *arg)
 {
@@ -35,47 +38,22 @@ int checked_strtol(char *arg)
     }
     return val;
 }
-void sigusr1_handler(int sigNo, siginfo_t *info, void *ucontext)
+void SIG_STUDENT_handler(int sigNo, siginfo_t *info, void *ucontext)
 {
-    lastsignal = SIGUSR1;
+    lastsignal = SIG_STUDENT;
     if (info)
-        lastsender = info->si_pid;
+    {
+        pending_students[pending_tail] = info->si_pid;
+        pending_tail = (pending_tail + 1) % 100;
+    }
 }
-void sigusr2_handler(int sigNo, siginfo_t *info, void *ucontext)
+void SIG_TEACHER_handler(int sigNo, siginfo_t *info, void *ucontext)
 {
-    lastsignal = SIGUSR2;
-    if (info)
-        lastsender = info->si_pid;
+    lastsignal = SIG_TEACHER;
 }
 void sigchld_handler(int sigNo, siginfo_t *info, void *ucontext)
 {
     lastsignal = SIGCHLD;
-    if (info)
-        lastsender = info->si_pid;
-    pid_t pid;
-    alive_children--;
-    while (1)
-    {   int status;
-        pid = waitpid(0, &status, WNOHANG);
-        
-        if (pid == 0)
-            return;
-
-        if (pid <= 0)
-        {
-            if (errno == ECHILD)
-                return; 
-            return;
-        }
-        else{
-            for(int i=0;i<childcount;i++){
-                if(student_pid[i]==info->si_pid){
-                    student_exit[i]=status;
-                    return;
-                }
-            }
-        }
-    }
 }
 
 void sethandler(void (*f)(int, siginfo_t *, void *), int sigNo)
@@ -83,7 +61,8 @@ void sethandler(void (*f)(int, siginfo_t *, void *), int sigNo)
     struct sigaction act;
     memset(&act, 0, sizeof(struct sigaction));
     act.sa_sigaction = f;
-    act.sa_flags = SA_SIGINFO;
+    act.sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESTART;
+    
     if (-1 == sigaction(sigNo, &act, NULL))
     {
         ERR("sigaction");
@@ -92,12 +71,18 @@ void sethandler(void (*f)(int, siginfo_t *, void *), int sigNo)
 
 void student_work(int no, int p, int t, int prob, sigset_t newmask)
 {
-    srand(getpid() % 15 * time(NULL));
+    srand(getpid() ^ time(NULL));
+
     struct timespec t1;
-    t1.tv_nsec = 100;
+    t1.tv_nsec = 100 * 1000 * 1000L;
     t1.tv_sec = 0;
+
     int isfailure;
     int issue_count = 0;
+    
+    sigset_t waitset;
+    sigfillset(&waitset);
+    sigdelset(&waitset, SIG_TEACHER);
 
     printf("Student[%d,%d] has started doing task!\n", no, getpid());
     for (int i = 0; i < p; i++)
@@ -105,66 +90,104 @@ void student_work(int no, int p, int t, int prob, sigset_t newmask)
         printf("Student[%d,%d] is starting doing part %d of %d\n", no, getpid(), i + 1, p);
         for (int y = 0; y < t; y++)
         {
+            t1.tv_nsec = 100 * 1000 * 1000L;
+            t1.tv_sec = 0;
             nanosleep(&t1, NULL);
-            isfailure = rand();
-            if (isfailure < prob)
+            isfailure = rand() % 100 + 1;
+
+            if (isfailure <= prob)
             {
                 issue_count++;
 
                 printf("Student[%d,%d] has issue (%d) doing task!\n", no, getpid(), issue_count);
+                t1.tv_nsec = 50 * 1000 * 1000L;
+                nanosleep(&t1, NULL);
             }
+        }
 
-            printf("Student[%d,%d] has finished doing part %d of %d\n", no, getpid(), i + 1, p);
-            lastsignal = 0;
-            kill(getppid(), SIGUSR1);
+        printf("Student[%d,%d] has finished doing part %d of %d\n", no, getpid(), i + 1, p);
+        lastsignal = 0;
+        kill(getppid(), SIG_STUDENT);
 
-            while (lastsignal != SIGUSR2)
-            {
-                sigsuspend(&newmask);
-                
-            }
+        while (lastsignal != SIG_TEACHER)
+        {
+            sigsuspend(&waitset);
         }
     }
 
     exit(issue_count);
 }
-int count_children(){
-    int cnt=0;
-    for(int i=0;i<childcount;i++){
-        if(student_exit[i]==-10){
+int count_children()
+{
+    int cnt = 0;
+    for (int i = 0; i < childcount; i++)
+    {
+        if (student_exit[i] == -10)
+        {
             cnt++;
-        }   
+        }
     }
     return cnt;
 }
-void teacher_work(int p, int t, int *prob, int prob_size, int * student_pid , sigset_t oldmask)
+
+void teacher_work(int p, int t, int *prob, int prob_size, int *student_pid, sigset_t oldmask)
 {
     for (int i = 0; i < prob_size; i++)
     {
-        pid_t p = 0;
-        if ((p = fork()) == 0)
+        pid_t child = 0;
+        if ((child = fork()) == 0)
         {
-            student_work(i, p, t, prob[i], oldmask);
+            student_work(i + 1, p, t, prob[i], oldmask);
         }
-        else{
-            student_pid[i]=p;
+        else
+        {
+            student_pid[i] = child;
         }
     }
-    while (alive_children>0)
+    while (alive_children > 0)
     {
 
         sigsuspend(&oldmask);
-        alive_children= count_children();
-        if(lastsignal == SIGUSR1){
-            kill(lastsender, SIGUSR2);
-        }
-        
-        lastsender = 0;
-        lastsignal = 0;
 
-        printf("Teacher has accepted solution of student [%d]\n", lastsender);
+        while (pending_head != pending_tail)
+        {
+            kill(pending_students[pending_head], SIG_TEACHER);
+            printf("Teacher has accepted solution of student [%d], alive children: %d\n", pending_students[pending_head], alive_children);
+            pending_head = (pending_head + 1) % 100;
+        }
+
+        pid_t pid;
+        int status;
+        
+        while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+        {
+            for (int i = 0; i < childcount; i++)
+            {
+                if (student_pid[i] == pid)
+                {
+                    student_exit[i] = WEXITSTATUS(status);
+                    break;
+                }
+            }
+        }
+
+        alive_children = count_children();
+        lastsignal = 0;
     }
 
+    while (wait(NULL) > 0)
+        ;
+
+    printf("No. | Student ID | Issue Count\n");
+    int total = 0;
+
+    for (int i = 0; i < prob_size; i++)
+    {
+        printf("%3d | %10d | %11d\n", i + 1, student_pid[i], student_exit[i]);
+        total += student_exit[i];
+    }
+
+    printf("Total issues: %d\n", total);
 }
 int main(int argc, char *argv[])
 {
@@ -173,40 +196,44 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s <arg1> <arg2> <arg3>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
     errno = 0;
     int p = checked_strtol(argv[1]);
     int t = checked_strtol(argv[2]);
     int cnt = 3;
 
     probabilities = malloc(sizeof(int) * (argc - 3));
-    student_pid = malloc(sizeof(int)*(argc-3));
-    student_exit = malloc(sizeof(int)*(argc-3));
-    childcount= argc-3;
-    alive_children=childcount;
-    
-    for(int i=0;i<childcount;i++){
-        student_exit[i]=-10;
+    student_pid = malloc(sizeof(int) * (argc - 3));
+    student_exit = malloc(sizeof(int) * (argc - 3));
+    childcount = argc - 3;
+    alive_children = childcount;
+
+    printf("%d\n", getpid());
+
+    for (int i = 0; i < childcount; i++)
+    {
+        student_exit[i] = -10;
     }
-  
+
     while (cnt < argc)
     {
         probabilities[cnt - 3] = checked_strtol(argv[cnt]);
         cnt++;
     }
-    sethandler(sigusr1_handler, SIGUSR1);
-    sethandler(sigusr2_handler, SIGUSR2);
+    sethandler(SIG_STUDENT_handler, SIG_STUDENT);
+    sethandler(SIG_TEACHER_handler, SIG_TEACHER);
     sethandler(sigchld_handler, SIGCHLD);
 
     sigset_t mask, oldmask;
     sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR2);
+    sigaddset(&mask, SIG_TEACHER);
     sigaddset(&mask, SIGCHLD);
-    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIG_STUDENT);
     sigprocmask(SIG_BLOCK, &mask, &oldmask);
 
-    teacher_work(p, t, probabilities, argc - 3, student_pid, mask);
+    teacher_work(p, t, probabilities, argc - 3, student_pid, oldmask);
 
-    sigprocmask(SIG_UNBLOCK, &mask, NULL);  
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
     free(probabilities);
     free(student_exit);
     free(student_pid);
